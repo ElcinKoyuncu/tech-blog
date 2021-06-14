a8757198a012c3634049754e87269ebb98d7aebd
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-06-14 18:57:31 -0500

    updating javascript files

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,11 @@
-function compareStrings(str1, str2) {
-    return str1.equals(str2);
-  }
-  
-  
-  module.exports = compareStrings;
+module.exports = {
+  format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+    return word;
+  }  
+};
