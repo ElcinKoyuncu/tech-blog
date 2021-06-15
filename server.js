76e881ae6d2e5a5c227c308c4783b57ae0e0fba8
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-06-14 19:21:21 -0500

    adding files

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
 const express = require('express');
+const sequelize = require('./config/connection');
+const path = require('path');
+const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-const path = require('path');
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-const PORT = process.env.PORT || 3001;
-
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({ db: sequelize })
-};
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+//setup session
+const sess = {
+  secret: 'supersecretsessionsecrettext',
+  cookie: { maxAge: 180000},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+//handlebars initialization
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+
+//use routes
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Client now listening on PORT ${PORT}`));
-})
+  app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
+});
